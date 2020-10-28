29b4e4d3621df6a6c14881b22e5766b61deddffe
jesscru <jessicacrump7@gmail.com>
2020-10-27 22:44:35 -0600

    added more pseudocode to readme, made function to start prompts after clicking generate buttom

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # passwordgenerator
+
+
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+<!-- onclick for the button that runs through a series of prompts -->
+<!-- user responses are stored in variables within the loop -->
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+<!-- use booleans to run a flow through if statements; stored in variables from prev step -->
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+<!-- acceptable numerical values of password length are to be ≥ 8 and ≤ 128 -->
+<!-- if statement -->
+WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters
+<!-- 4 global variables: lc, up, #, @-->
+<!-- if statement for prompt -->
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+<!-- huh? think this just means it should work? -->
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+<!-- should do final else as if they so no to everything: it should alert that they must allow at least one set of characters in order to generate a password -->
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+<!-- display the password in tthe provided box -->
--- script.js
+++ script.js
@@ -6,20 +6,27 @@ var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
 var specialCharArray = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/','\\', ':', ';', '<', '=', '>', '?', '@', '[', '\'', ']','^', '_', '`', '{', '|', '}', '~'];
 var numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
+function startPrompts() {
+  var confirmNumbers = confirm('Would you like your password to include numbers?');
+  var confirmLowercase = confirm('Would you like your password to include lowercase characters?');
+  var confirmUppercase = confirm('Would you like your password to include uppercase characters?');
+  var confirmSpecialChar = confirm('Would you like your password to include special characters (such as @#$%!)');
+}
 
-var confirmLowercase = confirm('Would you like your password to include lowercase characters?');
-var confirmUppercase = confirm('Would you like your password to include uppercase characters?');
-var confirmSpecialChar = confirm('Would you like your password to include special characters (such as @#$%!)')
-var confirmNumbers = confirm('Would you like your password to include numbers?')
+generateBtn.addEventListener("click", startPrompts);
 
+// if (confirmLowercase) {
+//   for (var i = 0; i < lowercaseArray.length; i++) {
 
+//   }
+// }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  // will do if statement on including the characters within the lowercaseArray
+
 
 
   passwordText.value = password;
